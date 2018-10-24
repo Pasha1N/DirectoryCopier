0786bd6696887cc9b7dc9da79530492f9ba2b5b0
Паша <@DESKTOP-3C2NDOK>
2018-10-24 20:02:52 +0300

    First commit.

---
+++ DirectoryCopier/View/MainWindow.xaml.cs
@@ -0,0 +1,15 @@
+﻿using DirectoryCopier.ViewModel;
+using System.Windows;
+
+namespace DirectoryCopier.View
+{
+
+    public partial class MainWindow : Window
+    {
+        public MainWindow(MainViewModel mainViewModel)
+        {
+            InitializeComponent();
+            DataContext = mainViewModel;
+        }
+    }
+}
---
+++ DirectoryCopier/ViewModel/MainViewModel.cs
@@ -0,0 +1,72 @@
+﻿using Microsoft.Win32;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Input;
+using DirectoryCopier.Command;
+using DirectoryCopier.Utilities;
+using System.Windows.Forms;
+
+namespace DirectoryCopier.ViewModel
+{
+    public class MainViewModel: EventINotifyPropertyChanged
+    {
+        private ICommand commandGetPathFrom;
+        private ICommand commandGetPathWhere;
+        private string pathFrom;
+        private string pathWhere;
+
+        public MainViewModel()
+        {
+            commandGetPathFrom = new DelegateCommand(GetPathFrom);
+            commandGetPathWhere = new DelegateCommand(GetPathWhere);
+        }
+
+        public ICommand CommandGetPathFrom => commandGetPathFrom;
+
+        public ICommand CommandGetPathWhere => commandGetPathWhere;
+
+        public string PathFrom
+        {
+            get => pathFrom;
+            set => SetProperty(ref pathFrom, value);
+        }
+
+        public string PathWhere
+        {
+            get => pathWhere;
+            set => SetProperty(ref pathWhere, value);
+        }
+
+        private void ReadFile()
+        {
+         
+            
+        }
+
+        private void GetPathFrom()
+        {
+            FolderBrowserDialog folderBrowserDialog = new FolderBrowserDialog();
+            folderBrowserDialog.ShowDialog();
+            PathFrom = folderBrowserDialog.SelectedPath;
+        }
+
+        private void GetPathWhere()
+        {
+            FolderBrowserDialog folderBrowserDialog = new FolderBrowserDialog();
+            folderBrowserDialog.ShowDialog();
+            PathWhere = folderBrowserDialog.SelectedPath;
+        }
+
+        private void WriteFile()
+        {
+
+
+        }
+
+
+    }
+}
