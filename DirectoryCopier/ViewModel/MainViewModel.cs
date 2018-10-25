f2a1a4f1071dca96633efe91d5a234b3e0692726
Паша <@DESKTOP-3C2NDOK>
2018-10-25 20:38:30 +0300

    Creating  method the copy files.

--- DirectoryCopier/ViewModel/MainViewModel.cs
+++ DirectoryCopier/ViewModel/MainViewModel.cs
@@ -12,12 +12,12 @@ using System.Windows.Forms;
 
 namespace DirectoryCopier.ViewModel
 {
-    public class MainViewModel: EventINotifyPropertyChanged
+    public class MainViewModel : EventINotifyPropertyChanged
     {
         private ICommand commandGetPathFrom;
         private ICommand commandGetPathWhere;
         private string pathFrom;
-        private string pathWhere;
+        private string pathTo;
 
         public MainViewModel()
         {
@@ -35,17 +35,12 @@ namespace DirectoryCopier.ViewModel
             set => SetProperty(ref pathFrom, value);
         }
 
-        public string PathWhere
+        public string PathTo
         {
-            get => pathWhere;
-            set => SetProperty(ref pathWhere, value);
+            get => pathTo;
+            set => SetProperty(ref pathTo, value);
         }
 
-        private void ReadFile()
-        {
-         
-            
-        }
 
         private void GetPathFrom()
         {
@@ -58,15 +53,82 @@ namespace DirectoryCopier.ViewModel
         {
             FolderBrowserDialog folderBrowserDialog = new FolderBrowserDialog();
             folderBrowserDialog.ShowDialog();
-            PathWhere = folderBrowserDialog.SelectedPath;
+            PathTo = folderBrowserDialog.SelectedPath;
+            CopyDirectry(PathTo);
         }
 
-        private void WriteFile()
+        private void CopyDirectry(string pathToCopy)
         {
+            string path = (string)pathToCopy;
+            DirectoryInfo directoryInfo = null;
+            DirectoryInfo[] directoryInfos = null;
+            FileInfo[] fileInfos = null;
+
+            if (path != null)
+            {
+                try
+                {
+                    directoryInfo = new DirectoryInfo(pathFrom);
+                    directoryInfos = directoryInfo.GetDirectories();
+                    fileInfos = directoryInfo.GetFiles();
+                    Directory.CreateDirectory(PathFrom);
+                }
+                catch (UnauthorizedAccessException)
+                {
+                }
+
+                if (fileInfos.Length > 0)
+                {
+                    foreach (FileInfo file in fileInfos)
+                    {
+                           File.Create(pathTo);
+                        // BinaryReader = new BinaryReader(); // как преобразовать файл в streamFile?
+                        CopyFiles(file.FullName, pathTo);
+                    }
+
+                }
 
 
+                //if (directoryInfos != null)
+                //{
+                //    foreach (DirectoryInfo directory in directoryInfos)
+                //    {
+                //        try
+                //        {
+                //            FileInfo[] currentDirectoryFiles = directory.GetFiles( );
+
+                //            foreach (FileInfo item in currentDirectoryFiles)
+                //            {
+
+                //            }
+                //        }
+                //        catch (UnauthorizedAccessException)
+                //        {
+                //        }
+
+                //        //GetDirectories(directory.FullName);
+                //    }
+                //}
+
+            }
         }
 
+        public void CopyFiles(FileInfo toFile, FileInfo fromFile)
+        {
+            using (BinaryReader binaryReader = new BinaryReader(File.Open(fromFile.Name, FileMode.Open)))
+            {
+                byte[] bytes = binaryReader.ReadBytes(4); //автоматически будет перемещаться?
 
+                BinaryWriterOfFiles(toFile, bytes);
+            }
+        }
+
+        private void BinaryWriterOfFiles(FileInfo toFile, byte[] bytes)
+        {
+            using (BinaryWriter binaryWriter = new BinaryWriter(File.Open(toFile.Name, FileMode.OpenOrCreate)))
+            {
+                binaryWriter.Write(bytes);
+            }
+        }
     }
 }
